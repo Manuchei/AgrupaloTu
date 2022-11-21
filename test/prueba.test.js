47df1d47798c6df8819c61477f3f46d492a526cd
Manuchei <manueldominguezgallego@gmail.com>
2022-11-21 09:49:21 +0100

    changes

--- test/prueba.test.js
+++ test/prueba.test.js
@@ -1,7 +1,10 @@
 import { describe, expect, it } from "vitest";
+import{createRandomGroups} from '../src/utils'
 
 describe("empty spec", () => {
   it("empty", () => {
+    const person = ['ivana', 'juana', 'ana', 'marina', 'diana']
+    const data = createRandomGroups(persons, 2)
     
     //expect(r).toBeInstanceOf(Number)
   });
